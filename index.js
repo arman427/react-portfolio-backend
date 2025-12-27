01cc44bfd5b63d51dc1033e994505dc47f6d6418
Arman <babayananuta11@gmail.com>
2025-12-27 17:12:49 +0300

    af

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 
+app.use(cors({
+   origin: ['https://react-portfolio-gules.vercel.app/', 'http://localhost:5173'],
+   credentials: true
+}));
+
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: false });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-app.use(cors());
 app.use(express.json());
 app.use(helmet()); // безопасность HTTP заголовков
 
+const PORT = process.env.PORT || 5000;
 const HOUR = 60 * 60 * 1000;
 
 const limiter = rateLimit({ //    запросов в час
